c01b030af789426c7e6e721d2c2ff8f1c3c0da2a
Cristian Alexoi <alexoi.cristian@gmail.com>
2020-04-22 23:58:31 +0300

    MessageDisplay
    Adaugat MenuListener.ShowMessage(text,durationInFrames)
    
    Ajustat meniul de ales promovarea

--- ChessAR/Assets/Scripts/MenuListener.cs
+++ ChessAR/Assets/Scripts/MenuListener.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuListener : MonoBehaviour
 {
@@ -10,17 +11,18 @@ public class MenuListener : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if (Input.GetKey(KeyCode.Escape) && timer>0.5) {
+        if (Input.GetKey(KeyCode.Escape) && timer > 0.5) {
             timer = 0;
             ToggleGameMenu();
         }
+        UpdateMessage();
     }
 
     public void RestartGame()
@@ -49,6 +51,33 @@ public class MenuListener : MonoBehaviour
         catch { }
     }
 
+    static private int _mframes = 0;
+    static private int _mframesActive = 0;
+    static private float _mframesInterval = 0.1f;
+    static private float _malpha = 0;
+    static private TMP_Text _txt;
+    private void UpdateMessage()
+    {
+        if (_mframes <= 0) return;
+        _mframes--;
+        if (_mframesActive > 0)
+        {
+            _mframesActive--;
+            return;
+        }
+        _malpha -= _mframesInterval;
+        _txt.alpha = _malpha;
+        //_txt.text = _malpha.ToString();
+    }
+    public static void ShowMessage(string message, int frames){
+        _txt = FindInActiveObjectByName("ShownText").GetComponent<TMP_Text>();
+        _mframes = frames;
+        _mframesActive = frames / 9;
+        _malpha = 1;
+        _mframesInterval = 1.0f / (_mframes - _mframesActive);
+        _txt.text = message;
+        _txt.alpha = 1;
+    }
     public static GameObject FindInActiveObjectByName(string name)
     {
         Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
@@ -66,11 +95,11 @@ public class MenuListener : MonoBehaviour
     }
     public void DebugButton(GameObject obj)
     {
-
+        MenuListener.ShowMessage("Mesaj",120);
         //var gameObject = Instantiate(obj);
         //PawnPicker.Prepare(gameObject); // setez pawn picker
-        
-        PawnPicker.Activate(DebugChoice);
+
+        //PawnPicker.Activate(DebugChoice);
     }
     private void DebugChoice(PieceType choice)
     {
